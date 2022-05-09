86b8b786be5992c82c0afabdb23b397b471e8460
xiaobaicai <861324312@qq.com>
2022-05-09 22:04:02 +0800

    注释内容

---
+++ toutiao-m-zq/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+   你管我？？？
+    <!-- 路由的出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less"></style>
